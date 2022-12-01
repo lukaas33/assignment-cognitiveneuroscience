26ca1e5ec73e33b528ae2b7b644ae7ee6e459366
lukaas33 <lucasvanosenbruggen@gmail.com>
2022-12-01 16:28:46 +0100

    Changed code arguments

--- number_recogniser.py
+++ number_recogniser.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.metrics import SparseCategoricalAccuracy
@@ -26,7 +26,10 @@ def get_data():
 
 # Preprocess data
 def preprocess(dataset, batchsize, shuffle=False):
-    dataset = dataset.map(lambda image, label: (cast(image, float32) / 255.0, label)) # Normalise pixel values
+    def normalise(image, label): 
+        return cast(image, float32) / 255.0, label
+
+    dataset = dataset.map(normalise) # Normalise pixel values
     if shuffle:
         dataset = dataset.shuffle(len(dataset)) # Shuffle to create random batches (instead of examples with the same label)
     dataset = dataset.batch(batchsize) # After each batch of some examples the error is calculated and the model trained; this improves runtime
@@ -34,15 +37,17 @@ def preprocess(dataset, batchsize, shuffle=False):
     return dataset
 
 # Define models and return them one by one
-def def_models():
-    # TODO add dropout?
+def def_models(layersize, convsize):
     yield (
         "OneLayerNN", # Simple one layered model
         Sequential([ # Feed forward NN
             Flatten(input_shape=(28, 28)), # Flatten 2D image matrix into 1D matrix; input layer with nodes for each pixel
             Dense( # Hidden layer, densely connected with previous layer
-                28*28, # same size as input layer; TODO try different sizes
+                layersize, # same size as input layer
                 activation='relu' # relu popular, avoid vanishing gradient problem; deactivates some neurons
+            ),
+            Dropout(
+                0.01 # Drop 1% of nodes, reduces overfitting
             ),
             Dense(10) # Output layer with a node for each number, densely connected with previous
         ])
@@ -51,9 +56,9 @@ def def_models():
         "ConvOneLayerNN", # Convolutional model
         Sequential([
             Conv2D( # convolutional layer TODO change and comment on parameters
-                28,
+                convsize,
                 kernel_size=3,
-                input_shape=(28, 28, 1), # Grayscale
+                input_shape=(28, 28, 1), # Grayscale, one channel instead of 3
                 activation="relu",
                 padding="same" # padding to ensure that the shape of the data stays the same
             ),
@@ -92,7 +97,7 @@ def train(ds_train, model, epochs, ds_test=None):
 
 
 # Main control flow
-def main(batchsize=128, epochs=2):
+def main(batchsize=128, epochs=2, layersize=250, convsize=10):
     # Get data
     ds_train, ds_test = get_data()
 
@@ -101,7 +106,7 @@ def main(batchsize=128, epochs=2):
     ds_test = preprocess(ds_test, batchsize)
 
     # Define models
-    models = def_models()
+    models = def_models(layersize, convsize)
 
     # Train and evaluate models
     best_model = None
@@ -109,15 +114,15 @@ def main(batchsize=128, epochs=2):
     time_prev = time()
     for name, model in models:
         model, score = train(ds_train, model, epochs, ds_test=ds_test) # Train and evaluate
-        accuracy = score[1]
-        loss = score[0]
-        runtime = time() - time_prev
-        time_prev = time()
+        loss, accuracy = score
         if accuracy > highest_acc: # Save best
             best_model = model
             highest_acc = accuracy
+        runtime, time_prev = time() - time_prev, time()
+        
         print(f"Model {name} was evaluated with an average accuracy of {round(accuracy, 3)} and a loss of {round(loss, 3)}. Runtime was {runtime}s")
     return best_model # Return best trained model
+
 
 if __name__ == '__main__':
     main()
